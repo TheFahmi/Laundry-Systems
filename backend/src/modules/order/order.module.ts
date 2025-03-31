4da3a2e1848deed587e3844b4d12d98c716c9f22
M Fahmi Hassan <me@mfah.me>
2025-03-31 21:33:28 +0700

    Register WorkOrder entities, service, and controller in OrderModule

--- backend/src/modules/order/order.module.ts
+++ backend/src/modules/order/order.module.ts
@@ -7,14 +7,29 @@ import { OrderItem } from './entities/order-item.entity';
 import { Payment } from '../payment/entities/payment.entity';
 import { Service } from '../service/entities/service.entity';
 import { AuthModule } from '../auth/auth.module';
+import { DailyJobQueue } from './entities/daily-job-queue.entity';
+import { JobQueueService } from './job-queue.service';
+import { JobQueueController } from './job-queue.controller';
+import { WorkOrder } from './entities/work-order.entity';
+import { WorkOrderStep } from './entities/work-order-step.entity';
+import { WorkOrderService } from './work-order.service';
+import { WorkOrderController } from './work-order.controller';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Order, OrderItem, Payment, Service]),
+    TypeOrmModule.forFeature([
+      Order, 
+      OrderItem, 
+      Payment, 
+      Service, 
+      DailyJobQueue, 
+      WorkOrder, 
+      WorkOrderStep
+    ]),
     AuthModule
   ],
-  controllers: [OrderController],
-  providers: [OrderService],
-  exports: [OrderService]
+  controllers: [OrderController, JobQueueController, WorkOrderController],
+  providers: [OrderService, JobQueueService, WorkOrderService],
+  exports: [OrderService, JobQueueService, WorkOrderService]
 })
 export class OrderModule {} 
